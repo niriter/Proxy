940a26a37981ca18de222a1e707f34132290a3ae
niriter <pro100.vce@gmail.com>
2024-08-20 23:52:32 +0300

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Proxy',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     description='A simple proxy module',
     install_requires=[
