80783ecdb21fbe64a76ee561a2da2bfe0b4920bf
niriter <pro100.vce@gmail.com>
2026-05-26 17:41:54 +0200

    Type-hinting

--- proxy/basic.py
+++ proxy/basic.py
@@ -3,7 +3,7 @@ from enum import Enum
 import requests
 import aiohttp
 
-from typing import Optional, Dict
+from typing import Optional, Dict, Union
 
 
 class ProxyType(str, Enum):
@@ -14,18 +14,18 @@ class ProxyType(str, Enum):
 
 
 class Proxy:
-    def __init__(self, raw_proxy: str, proxy_type: ProxyType = ProxyType.HTTP):
-        self.ip = ''
-        self.port = ''
-        self.username = ''
-        self.password = ''
-        self.type = proxy_type
+    def __init__(self, raw_proxy: str, proxy_type: ProxyType = ProxyType.HTTP) -> None:
+        self.ip: str = ''
+        self.port: str = ''
+        self.username: str = ''
+        self.password: str = ''
+        self.type: ProxyType = proxy_type
 
-        self.start_data = ''
+        self.start_data: str = ''
 
         self.load(raw_proxy)
 
-    def __str__(self):
+    def __str__(self) -> str:
         if self.username or self.password:
             return f'{self.type.value}://{self.username}:{self.password}@{self.ip}:{self.port}'
         return f'{self.type.value}://{self.ip}:{self.port}'
@@ -45,23 +45,23 @@ class Proxy:
         return None
 
     @property
-    def one_line_proxy(self):
+    def one_line_proxy(self) -> str:
         if self.username or self.password:
             return f'{self.username}:{self.password}@{self.ip}:{self.port}'
         return f'{self.ip}:{self.port}'
 
     @property
-    def requests(self):
+    def requests(self) -> Dict[str, str]:
         return {'http': self.full_url, 'https': self.full_url}
 
     @property
-    def aiohttp(self) -> Dict[str, str]:
-        data = {'proxy': self.url}
+    def aiohttp(self) -> Dict[str, Union[str, aiohttp.BasicAuth]]:
+        data: Dict[str, Union[str, aiohttp.BasicAuth]] = {'proxy': self.url}
         if self.auth:
             data['proxy_auth'] = self.auth
         return data
 
-    def load(self, proxy):
+    def load(self, proxy: str) -> None:
         self.start_data = proxy
         parsed = self.start_data.split(':')
         self.ip = parsed[0]
@@ -70,7 +70,7 @@ class Proxy:
             self.username = parsed[2]
             self.password = parsed[3]
 
-    def is_work(self):
+    def is_work(self) -> bool:
         try:
             ip = requests.get('https://api.my-ip.io/ip', proxies=self.requests, timeout=2).text
             print(ip)
@@ -81,5 +81,5 @@ class Proxy:
             print(e)
             return False
 
-    def id(self):
+    def id(self) -> str:
         return f'{self.ip.replace(".", "")}{self.port}'
